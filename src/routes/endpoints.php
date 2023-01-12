a30f2daa97ec7e8db4478348aa3d9c8d123ac94f
Carlos Miño Flores <calolomino@gmail.com>
2023-01-12 14:12:44 -0500

    ac

--- src/routes/endpoints.php
+++ src/routes/endpoints.php
@@ -5906,9 +5906,11 @@ $app->group('/api', function() use ($app) {
                     FROM notas_registros R
                     LEFT JOIN notas_registros_estados E
                     ON R.estado = E.id_estados
-                    WHERE (R.banco=".$idBanco.") AND (R.identificador='".$identificador."') 
+                    WHERE (R.banco=".$idBanco.") AND (R.identificador='".$identificador."') AND (DATE(R.fecha_ultima_contacto) BETWEEN '".$from."' AND '".$to."')
                     GROUP BY R.estado
                     ORDER BY COUNT(R.estado) DESC";
+
+                    $respuesta['sql'] = $sql;
 
                     $porEstado = $mysql->Consulta($sql);
 
