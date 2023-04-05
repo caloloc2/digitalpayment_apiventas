09725f6ee9780c20d65d965bfc495ed746bdbc88
Carlos Miño Flores <calolomino@gmail.com>
2023-04-05 17:18:40 -0500

    ac

--- src/routes/endpoints.php
+++ src/routes/endpoints.php
@@ -9901,7 +9901,7 @@ $app->group('/api', function() use ($app) {
                     }
 
                     $consulta = $mysql->Consulta("SELECT
-                    R.id_lead, R.ruc, R.comercio, R.propietario, C.ciudad, R.direccion, R.telefono, R.celular, R.fechaAlta, R.fechaModificacion, R.estado, E.descripcion, E.color, E.icono
+                    R.id_lead, R.ruc, R.comercio, R.propietario, C.ciudad, R.direccion, R.telefono, R.celular, R.fechaAlta, R.fechaModificacion, R.estado, E.descripcion, E.color, E.icono, E.linkeable
                     FROM registros_internacional R
                     LEFT JOIN registros_internacional_ciudades C
                     ON R.id_ciudad = C.id_ciudad
@@ -9929,6 +9929,7 @@ $app->group('/api', function() use ($app) {
                                         "descripcion" => $linea['descripcion'],
                                         "color" => $linea['color'],
                                         "icono" => $linea['icono'],
+                                        "linkeable" => (bool) $linea['linkeable']
                                     ),
                                 ));
                             }
