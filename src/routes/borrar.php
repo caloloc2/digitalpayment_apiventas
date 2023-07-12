f9b61b85043b07f9664d1759c70f663d12027941
Carlos Miño Flores <calolomino@gmail.com>
2023-07-11 23:29:20 -0500

    ac

--- src/routes/borrar.php
+++ src/routes/borrar.php
@@ -1,13 +1,19 @@
 <?php 
 
 $folder = __DIR__."/../../public/estab/1722653894001";
+$url = "https://api.digitalpaymentnow.com/estab/1722653894001";
 
+$listado = [];
 $d = dir($folder);
-
 while (false !== ($entry = $d->read())){
     if (!is_dir($entry) && ($entry != '.') && ($entry != '..')){
-        echo $entry."\n";
+        // echo $entry."\n";
+        array_push($listado, array(
+            "link" => $url."/".$entry
+        ));
     }
 }
 $d->close();
 // print_r($listado);
+
+echo json_encode($listado);
