c5ad3a1edb6e400e20542c2bfbfecb850dd16570
Carlos Miño Flores <calolomino@gmail.com>
2023-07-11 23:24:12 -0500

    ac

--- src/routes/borrar.php
+++ src/routes/borrar.php
@@ -1,7 +1,7 @@
 <?php 
 
 $folder = __DIR__."/../../public/estab/1722653894001";
+echo file_exists($folder);
+// $listado = dir($folder);
 
-$listado = dir($folder);
-
-print_r($listado);
+// print_r($listado);
