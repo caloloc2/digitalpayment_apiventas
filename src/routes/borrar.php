2279ec58a0e930a09b45f47b5fb1290968b25a52
Carlos Miño Flores <calolomino@gmail.com>
2023-07-11 23:27:54 -0500

    ac

--- src/routes/borrar.php
+++ src/routes/borrar.php
@@ -5,7 +5,7 @@ $folder = __DIR__."/../../public/estab/1722653894001";
 $d = dir($folder);
 
 while (false !== ($entry = $d->read())){
-    if (is_dir($entry) && ($entry != '.') && ($entry != '..')){
+    if (!is_dir($entry) && ($entry != '.') && ($entry != '..')){
         echo $entry."\n";
     }
 }
